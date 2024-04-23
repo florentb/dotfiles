5565740b3efdca5c2b4dda9887becf313b485009
Florent Bourgeois <florent@twic.pics>
2024-04-23 11:51:33 +0200

    Replaces Fig by Codewhisper

---
+++ codewhisperer/path.zsh
@@ -0,0 +1,2 @@
+# CodeWhisperer post block.
+[[ -f "${HOME}/Library/Application Support/codewhisperer/shell/zshrc.post.zsh" ]] && builtin source "${HOME}/Library/Application Support/codewhisperer/shell/zshrc.post.zsh"
